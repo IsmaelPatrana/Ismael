00c6e1a8dce344fe1de10f2bc27653f83b662315
IsmaelBack <72406061+IsmaelPatrana@users.noreply.github.com>
2021-05-22 11:53:27 -0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+
+
+function button () {
+  Swal.fire({
+    icon: 'alert',
+    title: 'Mi correo',
+    text: 'Pastranadeveloper@gmail.com',
+    background: '#fff url()',
+    backdrop: `
+    rgba(0,0,123,0.4)
+    url("")
+    center
+    
+    no-repeat
+  `
+    
+  })
+  
+}
